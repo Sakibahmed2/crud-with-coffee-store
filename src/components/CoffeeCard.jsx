da2cf074cfca9883da598f604baac5e9d8f35583
Sakibahmed2 <mauijaloskor@gmail.com>
2023-05-08 18:47:41 +0600

    coffee store done

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
-const CoffeeCard = ({ coffee }) => {
+const CoffeeCard = ({ coffee, coffees, setCoffees }) => {
 
     const { _id, name, quantity, supplier, taste, category, details, photo } = coffee;
 
@@ -17,11 +18,23 @@ const CoffeeCard = ({ coffee }) => {
             confirmButtonText: 'Yes, delete it!'
         }).then((result) => {
             if (result.isConfirmed) {
-                Swal.fire(
-                    'Deleted!',
-                    'Your file has been deleted.',
-                    'success'
-                )
+
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                            const remaning = coffees.filter(cof => cof._id !== _id)
+                            setCoffees(remaning)
+                        }
+                    })
             }
         })
     }
@@ -41,7 +54,9 @@ const CoffeeCard = ({ coffee }) => {
                     <div className="card-actions justify-end">
                         <div className="btn-group btn-group-vertical space-y-4 p-4">
                             <button className="btn ">Veiw</button>
-                            <button className="btn">Edit</button>
+                            <Link to={`/updateCoffee/${_id}`}>
+                                <button className="btn">Edit</button>
+                            </Link>
                             <button onClick={() => handleDelete(_id)} className="btn ">X</button>
                         </div>
                     </div>
